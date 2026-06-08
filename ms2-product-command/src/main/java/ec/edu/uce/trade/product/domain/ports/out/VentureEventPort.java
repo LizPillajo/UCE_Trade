bccbf4d0f566dac21a7b1c9fa1e595caffe298e0
LizPillajo <ldpillajo@uce.edu.ec>
2026-06-08 02:16:58 -0500

    fix: remove duplicated backend files and folders from root

--- ms2-product-command/src/main/java/ec/edu/uce/trade/product/domain/ports/out/VentureEventPort.java
+++
@@ -1,7 +0,0 @@
-package ec.edu.uce.trade.product.domain.ports.out;
-
-import ec.edu.uce.trade.product.domain.model.Venture;
-
-public interface VentureEventPort {
-    void publishVentureCreatedEvent(Venture venture);
-}
